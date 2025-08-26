3e3c5e01fa3635d160ef7aff415c55c4876d426b
NITESH <117387231+N123itesh@users.noreply.github.com>
2025-08-26 13:46:33 +0000

    operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,11 +1,11 @@
 let score="33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 //typeconversion=>
@@ -19,7 +19,7 @@ console.log(valueInNumber);
 
 let isLoggedIn=1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1=>true; 0=>false
 //""=> false
@@ -27,6 +27,29 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//**************************************Operations******************************************/
+
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/3);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1="hello"
+let str2= " Nitesh"
+let str3= str1+str2
+console.log(str3);
+
+
 
