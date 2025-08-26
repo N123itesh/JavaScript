fb51d0893d40c801ef520a0e94a9f9471b60d678
NITESH <117387231+N123itesh@users.noreply.github.com>
2025-08-26 17:39:01 +0000

    operation

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -49,7 +49,16 @@ let negvalue = -value
 let str1="hello"
 let str2= " Nitesh"
 let str3= str1+str2
-console.log(str3);
+// console.log(str3);
+
+console.log(1 +"2")
+console.log("1" + 2)
+console.log(1 + 2 + "2")
+console.log("1" + 2 + 2)
+
+
+
+
 
 
 
