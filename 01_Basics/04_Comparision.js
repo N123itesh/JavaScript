0d0258a626b9b9e390624feac7511a82ae0a5345
NITESH <117387231+N123itesh@users.noreply.github.com>
2025-08-26 18:03:35 +0000

    COMPARISION

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,28 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+/************************************************SUMMARY*********************************************/
+
+/*
+
+TWO TYPES OF DATATYPES ====>>>>
+==> PREMETIVE DATA TYPES 
+====>7TYPES=>STRINGS , NUMBER , BOOLEAN , NULL , UNDEFINE , SYBBOL , BIGINT;
+
+==> REFERENCE (NON PREMITIVE)
+====> ARRAY , OBJECT , FUNCTIONS;
+
+*/
