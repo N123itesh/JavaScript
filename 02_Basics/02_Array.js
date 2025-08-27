b852d117e85dd2a9e1ca5c11411e7fb9c6584476
NITESH <117387231+N123itesh@users.noreply.github.com>
2025-08-27 19:41:08 +0000

    Array and objects

---
+++ 02_Basics/02_Array.js
@@ -0,0 +1,35 @@
+const marvel_heros = ["Thor", "Ironman", "Hulk"]
+const DC_heros = ["superman" , "Flash", "Batman"]
+
+// marvel_heros.push(DC_heros)
+// console.log(marvel_heros);
+// console.log(marvel_heros[3]);
+// console.log(marvel_heros[3][1]);
+
+
+// const all_heros = marvel_heros.concat(DC_heros)
+// console.log(all_heros);
+
+
+// const all_new_heros = [...marvel_heros, ...DC_heros]
+// console.log(all_new_heros);
+
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_array = another_array.flat(Infinity)
+// console.log(real_array);
+
+
+console.log(Array.isArray("Nitesh"));
+console.log(Array.from("Nitesh"));
+console.log(Array.from({name: "Nitesh"}));
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+ 
+console.log(Array.of(score1,score2,score3));
+
+
+
